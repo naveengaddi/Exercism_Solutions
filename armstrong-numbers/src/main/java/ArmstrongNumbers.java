52f380ef0af00ac7ebd13b5a3e4b30c0d2103cc3
Naveen <naveenyadav6256@gmail.com>
2019-12-26 15:15:18 +0530

    [Naveen Gaddi] Updated Armstrong code

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -3,22 +3,15 @@ class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
 
-		 int length = String.valueOf(numberToCheck).length();
-		// int num = numberToCheck;
-		// int sum = 0;
-		// while(num>0){
-		// 	sum += Math.pow(num%10,length);
-		// 	num = num/10;
-		// }
-		// if(numberToCheck == sum )
-		// 	return true;
-		// return false;
-		String s = String.valueOf(numberToCheck);
-		int sum1 = 0;
-		Arrays.stream(s).mapToInt(i->i.intValue()).sum();
-		if(sum1 == numberToCheck){
-			return true;
+		int length = String.valueOf(numberToCheck).length();
+		int num = numberToCheck;
+		int sum = 0;
+		while(num>0){
+			sum += Math.pow(num%10,length);
+			num = num/10;
 		}
+		if(numberToCheck == sum )
+			return true;
 		return false;
 	}
 
