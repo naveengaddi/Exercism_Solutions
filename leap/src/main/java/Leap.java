fb600d4bb93986b090c863a7df80cefd54694017
Naveen <naveenyadav6256@gmail.com>
2019-12-26 15:20:39 +0530

    [Naveen Gaddi] Updated Leap Year code

--- leap/src/main/java/Leap.java
+++ leap/src/main/java/Leap.java
@@ -1,7 +1,19 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(year%4==0){
+        	if(year%100==0){
+        		if(year%400==0){
+        			return true;
+        		}else{
+        			return false;
+        		}
+        	}else{
+        		return true;
+        	}
+        }else{
+        	return false;
+        }
     }
 
 }
